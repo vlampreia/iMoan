f530a3eca555a7b6bbaf266bf12d5ca1b2bf0c24
vlampreia <victorlampreia@gmail.com>
2015-11-22 16:30:08 +0100

    Code cleanup

--- src/iMoan.c
+++ src/iMoan.c
@@ -9,32 +9,44 @@
 #include <IOKit/pwr_mgt/IOPMLib.h>
 #include <IOKit/IOMessage.h>
 
-io_connect_t root_port;
+const char *WAVEFORM_PATH = "scream.wav";
 
-void MyMoanCallBack(void *refCon, io_service_t service,
-                    natural_t messageType, void *messageArgument) {
-
+void MyMoanCallBack(
+    void *refCon,
+    io_service_t service,
+    natural_t messageType,
+    void *messageArgument
+) {
     if (messageType == kIOMessageSystemWillPowerOn) {
-        execlp("afplay", "afplay", "scream.wav", (char *)NULL);
+        execlp("afplay", "afplay", WAVEFORM_PATH, (char *)NULL);
     }
 }
- 
+
 int main(int argc, char **argv) {
+    io_connect_t root_port;
     IONotificationPortRef notifyPortRef;
     io_object_t notifierObject;
     void *refCon = NULL;
- 
-    root_port = IORegisterForSystemPower(refCon, &notifyPortRef, MyMoanCallBack, &notifierObject);
+
+    root_port = IORegisterForSystemPower(
+        refCon,
+        &notifyPortRef,
+        MyMoanCallBack,
+        &notifierObject
+    );
 
     if (root_port == 0) {
         printf("IORegisterForSystemPower failed\n");
         return 1;
     }
- 
-    CFRunLoopAddSource(CFRunLoopGetCurrent(),
-                       IONotificationPortGetRunLoopSource(notifyPortRef), kCFRunLoopCommonModes );
+
+    CFRunLoopAddSource(
+        CFRunLoopGetCurrent(),
+        IONotificationPortGetRunLoopSource(notifyPortRef),
+        kCFRunLoopCommonModes
+    );
 
     CFRunLoopRun();
- 
+
     return (0);
 }
